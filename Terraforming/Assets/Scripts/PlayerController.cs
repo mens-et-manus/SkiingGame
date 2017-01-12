f877b62a63dfe4ab24b67516c8d08afff8b83997
Darius Bopp <darbopp@gmail.com>
2017-01-12 14:43:50 -0500

    skiing Game
    a game of skiing down a hill

---
+++ Terraforming/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(-5*Time.deltaTime, 0f, Input.GetAxis("Horizontal")*5*Time.deltaTime);
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
